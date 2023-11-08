8f6b30e02fa454ad75294f1056935678e1932175
JuanesG22 <102272125+JuanesG22@users.noreply.github.com>
2023-11-08 10:04:26 -0500

    Details

--- SQL Temperature Project/TemperatureMajorCities Portfolio Project.sql
+++ SQL Temperature Project/TemperatureMajorCities Portfolio Project.sql
@@ -31,7 +31,7 @@ SELECT *
 FROM TemperatureMajorCities..city_temperature
 WHERE State is Null 
 
--- On State column is empty for all the countries except US 
+-- State column is empty for all the countries except US 
 
 SELECT * 
 FROM TemperatureMajorCities..city_temperature
@@ -68,7 +68,7 @@ FROM TemperatureMajorCities..city_temperature
 GROUP BY CAST(Year AS int)
 ORDER BY CAST(Year AS int) asc
  
--- Query above shows the 200 and 201 year which does not make sense. Let's see all the data related to these values:
+-- Query above shows the 200 and 201 years which do not make sense. Let's see all the data related to these values:
 
 SELECT *
 FROM TemperatureMajorCities..city_temperature
@@ -93,7 +93,7 @@ SELECT *
 FROM TemperatureMajorCities..city_temperature
 WHERE CAST(Day AS int) = 0
 
--- Same thing that before, the Select above shows missing values
+-- Same thing that before, the query above shows missing values
 
 --  Select the AvgTemperature values equals to -99
 
@@ -101,9 +101,9 @@ SELECT *
 FROM TemperatureMajorCities..city_temperature
 WHERE CAST(AvgTemperature AS float) = -99
 
--- So there is 79672 of 2906327 with -99 of AvgTemperature, the 2.74% of the dataset are missing values by -99 indicator.
+-- So there are 79672 of 2906327 with -99 of AvgTemperature, the 2.74% of the dataset are missing values by -99 indicator.
 
---  What we going to do know is to find how many cities are registered, with these number we can calculate the total data that the dataset should have
+-- What we are going to do now is to find how many cities are registered, with this number we can calculate the total data that the dataset should have
 
 SELECT Region,Country,State, City
 FROM TemperatureMajorCities..city_temperature
@@ -112,10 +112,10 @@ ORDER BY 1,2
 
 /*
 There is 325 different cities. We already know that the collection period of the data is between 1995-2020 (26 years). In this order, if the collection has been done by year the
-lenght of the data should be 8450 or in other words, there should be 8450 registers (325x26). Following through, by month 101400 registers (325x26x12) and last by day 37011000  
+lenght of the data should be 8450 or in other words, there should be 8450 registers (325x26). Following through, by month, 101400 registers (325x26x12) and last by day 37011000  
 registers (325x26x12x365) without including the extra days because of the leap years (in this period is 7 days). Yet, the dataset has 2906327 (considering the -99 missing values)
 records.
-So next, the idea is to select those cities that has all the records for every year, calculate the average temperature for each year and insert it into a view.
+So next, the idea is to select those cities that have all the records for every year, calculate the average temperature for each year and insert it into a view.
 */
 
 SELECT City,Year
@@ -223,12 +223,12 @@ END AS MonthAxis
 FROM MinMaxYearByDay
 
 /*
-There is something special that I want to mention about the above views. At the beggining of this script, it was show that the dataset has a lot of missing data. The missing data is 
-not only related to one specific column, in fact, the columns with missing values for our interest are Year, Month, Day and AvgTemperature. Looking deep, some queries before it was 
+There is something special that I want to mention about the above views. At the beggining of this script, it was showed that the dataset has a lot of missing data. The missing data is 
+not only related to one specific column, in fact, the columns with missing values for our interest are Year, Month, Day and AvgTemperature. Looking deep, some queries before
 showed that the total data expected is 37011000, and the dataset represent (without the -99 AvgTemperature Values) the 7,6% of the total expected data, which means that there 
-is a lot of missing data. By the other hand, the intention with the views that were created is to show the avg temperature by month and the min and max avgTemperature by month over 
+is a lot of missing data. On the other hand, the intention with the views that were created is to show the avg temperature by month and the min and max avgTemperature by month over 
 the period 1995-2020 on every city. So we created the CTE FullCountries that has the cities with all the years and all the months, yet, this isn't true at all, and the query below 
-will explain that. The FullCountries CTE has a column call it NumberOfMonths, because it is not partition by at the time to made the count over the months, if a country has a year with
+will explain that. The FullCountries CTE has a column call it NumberOfMonths, because it is not partition by at the time to make the count over the months, if a country has a year with
 5 months of data and the other 25 years with 12 months of data, when the query calculates the avgTemperature it would pass away from the missing data and continue with the calculation. 
 This is the reason why the views are not empty. The query below calculates the total months that every city has in the period 1995-2020. If, the data from a city is complete at the 
 month column, the sum of months partition by city will show 78*26 = 2028. (78 because if we sum the month values (1+2+...12 =78). The result in AllMonthsAllYears shows that there is 
